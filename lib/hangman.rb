1794c7c39b7bef03d5ca09831a3c9cc759d63b0d
HereIsSee <biramantvydas@gmail.com>
2024-07-27 16:30:59 +0300

    Add method to choose hangman secret word
    Added a method that selects word from word array and
    a method to read from file.

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -1,4 +1,23 @@
+
 class Hangman
 
+  def initialize
+
+  end
+
+  def select_secret_word array_of_words
+    loop do
+      index = rand(0...array_of_words.length)
+      if array_of_words[index].length >= 5 && array_of_words[index].length <=12
+        return array_of_words[index]
+      end
+    end
+  end
+
+ 
 
 end
+def read_from_file string
+  File.read(string).split(" ")
+end
+p Hangman.new.select_secret_word( read_from_file("words.txt") )
