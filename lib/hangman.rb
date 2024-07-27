9273cd6dbb30cd86a2cf2656842c2405637105c7
HereIsSee <biramantvydas@gmail.com>
2024-07-27 16:52:46 +0300

    Add guess_letter method
    Added the guess letter method for the hangman class
    which is used to guess a letter.

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -2,7 +2,10 @@
 class Hangman
 
   def initialize
-
+    @guess_counter = 10
+    @secret_word = select_secret_word( read_from_file("words.txt") )
+    @guesed_word = "_" * @secret_word.length
+    @already_guessed_letters = Array.new
   end
 
   def select_secret_word array_of_words
@@ -14,10 +17,33 @@ class Hangman
     end
   end
 
- 
+  def guess_letter
+    puts "You have #{@guess_counter} guesses left"
+    puts "Guess letter:"
+    
+    loop do
+      guessed_letter = gets.chomp
+
+      if guessed_letter.length != 1 || !guessed_letter.match?(/[[:alpha:]]/)
+        puts "Incorrect input, try again!"
+      elsif @already_guessed_letters.include?(guessed_letter)
+        puts "You have already guessed this letter!"
+      else
+        @already_guessed_letters.push guessed_letter
+        return guessed_letter
+      end
+
+    end
+    
+  end
 
 end
+
 def read_from_file string
   File.read(string).split(" ")
 end
-p Hangman.new.select_secret_word( read_from_file("words.txt") )
+
+var = Hangman.new()
+
+var.guess_letter
+var.guess_letter
